bc96ae2cb3fe880679eb20e52260670d22e8c9e3
loxt <emannuelloxt@hotmail.com>
2022-04-16 19:12:44 -0400

    fix(test): product return error when try to enable

--- adapters/cli/product_test.go
+++ adapters/cli/product_test.go
@@ -13,7 +13,7 @@ func TestRun(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	productName := "novo produto"
+	productName := "Product Test"
 	productPrice := 25.99
 	productStatus := "enabled"
 	productId := "abc"
@@ -21,25 +21,35 @@ func TestRun(t *testing.T) {
 	productMock := mock_application.NewMockProductInterface(ctrl)
 	productMock.EXPECT().GetID().Return(productId).AnyTimes()
 	productMock.EXPECT().GetStatus().Return(productStatus).AnyTimes()
-	productMock.EXPECT().GetName().Return(productName).AnyTimes()
 	productMock.EXPECT().GetPrice().Return(productPrice).AnyTimes()
+	productMock.EXPECT().GetName().Return(productName).AnyTimes()
 
 	service := mock_application.NewMockProductServiceInterface(ctrl)
 	service.EXPECT().Create(productName, productPrice).Return(productMock, nil).AnyTimes()
 	service.EXPECT().Get(productId).Return(productMock, nil).AnyTimes()
-	service.EXPECT().Enable(productId).Return(productMock, nil).AnyTimes()
-	service.EXPECT().Disable(productId).Return(productMock, nil).AnyTimes()
+	service.EXPECT().Enable(gomock.Any()).Return(productMock, nil).AnyTimes()
+	service.EXPECT().Disable(gomock.Any()).Return(productMock, nil).AnyTimes()
 
-	resultExpected := fmt.Sprintf("Product ID %s with the name %s and price %f has been created", productId, productName, productPrice)
-
+	resultExpected := fmt.Sprintf("Product ID %s with the name %s has been created with the price %f and status %s",
+		productId, productName, productPrice, productStatus)
 	result, err := cli.Run(service, "create", "", productName, productPrice)
 	require.Nil(t, err)
 	require.Equal(t, resultExpected, result)
 
-	resultExpected = fmt.Sprintf("Product %s has been enabled", productName)
-	result, err = cli.Run(service, "enable", productId, "", 25)
-	t.Logf("error %s", err)
-	////require.Nil(t, err)
-	////require.Equal(t, resultExpected, result)
+	resultExpected = fmt.Sprintf("Product %s has been enabled.", productName)
+	result, err = cli.Run(service, "enable", productId, "", 0)
+	require.Nil(t, err)
+	require.Equal(t, resultExpected, result)
+
+	resultExpected = fmt.Sprintf("Product %s has been disabled.", productName)
+	result, err = cli.Run(service, "disable", productId, "", 0)
+	require.Nil(t, err)
+	require.Equal(t, resultExpected, result)
+
+	resultExpected = fmt.Sprintf("Product ID: %s\nName: %s\nPrice: %f\nStatus: %s",
+		productId, productName, productPrice, productStatus)
+	result, err = cli.Run(service, "get", productId, "", 0)
+	require.Nil(t, err)
+	require.Equal(t, resultExpected, result)
 
 }
