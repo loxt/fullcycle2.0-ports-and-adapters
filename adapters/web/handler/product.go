3fffb4266b0a9cd6c466049f25a5d239361fdb55
loxt <emannuelloxt@hotmail.com>
2022-04-16 21:46:11 -0400

    feat: add create handler

--- adapters/web/handler/product.go
+++ adapters/web/handler/product.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"github.com/loxt/fullcycle2.0-ports-and-adapters/adapters/dto"
 	"github.com/loxt/fullcycle2.0-ports-and-adapters/application"
 	"github.com/urfave/negroni"
 	"net/http"
@@ -12,6 +13,10 @@ func MakeProductHandlers(r *mux.Router, n *negroni.Negroni, service application.
 	r.Handle("/product/{id}", n.With(
 		negroni.Wrap(getProduct(service)),
 	)).Methods("GET", "OPTIONS")
+
+	r.Handle("/product/", n.With(
+		negroni.Wrap(createProduct(service)),
+	)).Methods("POST")
 }
 
 func getProduct(service application.ProductServiceInterface) http.Handler {
@@ -25,12 +30,38 @@ func getProduct(service application.ProductServiceInterface) http.Handler {
 			w.WriteHeader(http.StatusNotFound)
 			return
 		}
-
 		err = json.NewEncoder(w).Encode(product)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+	})
+}
 
+func createProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		var productDto dto.Product
+
+		err := json.NewDecoder(r.Body).Decode(&productDto)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write(jsonError(err.Error()))
+			return
+		}
+
+		product, err := service.Create(productDto.Name, productDto.Price)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write(jsonError(err.Error()))
+			return
+		}
+		err = json.NewEncoder(w).Encode(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write(jsonError(err.Error()))
+			return
+		}
 	})
 }
